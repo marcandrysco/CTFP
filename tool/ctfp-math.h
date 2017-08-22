4286065d07b2016e51cc916728fbc7fb956396b7
Marc Andrysco <marc.andrysco@gmail.com>
2017-08-22 12:01:34 -0700

    should work on musl, starting math library

---
+++ tool/ctfp-math.h
@@ -0,0 +1,14 @@
+#ifndef CTFP_MATH_H
+#define CTFP_MATH_H
+
+float ctfp__cosdf(double x);
+float ctfp__cosdf(double x);
+
+double ctfp_scalb(double x, double fn);
+double ctfp_scalbn(double x, int n);
+float ctfp_scalbnf(float x, int n);
+
+double ctfp_exp(double x);
+float ctfp_expf(float x);
+
+#endif
